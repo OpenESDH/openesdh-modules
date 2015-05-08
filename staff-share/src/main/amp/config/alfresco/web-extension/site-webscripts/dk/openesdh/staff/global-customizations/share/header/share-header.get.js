fab910cbfbfbf0131f392c9c60563a36cea7d2bb
Torben N. Rasmussen <Torben.Rasmussen@visma.com>
2015-05-08 11:19:20 +0200

    Added missing file

---
+++ staff-share/src/main/amp/config/alfresco/web-extension/site-webscripts/dk/openesdh/staff/global-customizations/share/header/share-header.get.js
@@ -0,0 +1,10 @@
+<import resource="classpath:/alfresco/web-extension/site-webscripts/dk/openesdh/utils/case.js">
+
+addAdditionalCreateCaseDialog("staff", [
+   {
+       name: "alfresco/buttons/AlfButton",
+       config: {
+           label: "staff"
+       }
+   }
+]);
